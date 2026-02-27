b62c4113e38593af5059c68147508b18c79cb745
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2026-02-27 16:07:37 +0530

    Update stringRev.py

--- stringRev.py
+++ stringRev.py
@@ -1,6 +1,9 @@
 string = input("Enter a string:")
 #reverse a string without using slicing
 rev=""
-for i in range(len(string)-1,-1,-1):
+#using while loop
+i=len(string)-1
+while(i>=0):
     rev+=string[i]
+    i-=1
 print(rev)
