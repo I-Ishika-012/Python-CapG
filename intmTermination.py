5b7d8e641fdcea2c808190e9fa41209a377213d7
Ishika Dutta <91879691+I-Ishika-012@users.noreply.github.com>
2026-02-28 12:54:26 +0530

    added files

---
+++ intmTermination.py
@@ -0,0 +1,22 @@
+# INTERMEDIATE TERMINATION CONDITION
+#BREAK
+#PASS
+#CONTINUE
+
+#Break
+for i in range(1, 11):
+    if i==5:
+        break
+    print(i)
+
+#Continue
+for i in range(1, 11):
+    if i==5:
+        continue
+    print(i)
+
+#Pass
+for i in range(1, 11):
+    if i==5:
+        pass
+    print(i)
